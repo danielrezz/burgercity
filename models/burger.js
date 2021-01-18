ad679c944f2f4a4d1ef46c653d872290ffdddf5d
Daniel Reza <daniel.reza1007@gmail.com>
2021-01-18 14:13:09 -0600

    model setup

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,27 @@
+const express = require("express");
 const orm = require("../config/orm.js");
 
-orm
+let burgerModel = {
+    all: async function() {
+
+        const results = await orm.selectAll("burgers");
+        
+        return results;
+    },
+
+    create: async function(cols, vals, cb) {
+
+        const results = await orm.insertOne("burgers", cols, vals);
+
+        return results;
+    },
+
+    update: async function(objColVals, condition, cb) {
+
+        const results = await orm.updateOne("burgers", objColVals, condition);
+
+       return results;
+    }
+};
+
+module.exports = burgerModel;
