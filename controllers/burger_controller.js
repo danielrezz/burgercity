42862fdc9fbf0b0a630d505855fc2724b6feb57f
Daniel Reza <daniel.reza1007@gmail.com>
2021-01-18 22:16:01 -0600

    updating controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,5 +1,28 @@
 const express = require("express");
-const router = express.Router();
 const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+
+router.get("/", async function(req, res) {
+
+
+    const result = await burger.all(data)
+
+
+});
+
+// router.get("/", function(req, res) {
+//     cat.all(function(data) {
+//       var hbsObject = {
+//         cats: data
+//       };
+//       console.log(hbsObject);
+//       res.render("index", hbsObject);
+//     });
+//   });
+
+
+
 
 module.exports = router;
